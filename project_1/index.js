bafe252dadebf25b507c9575bedfb51ee5b0f4aa
Wallace <wallaceedua@gmail.com>
2021-03-20 23:12:06 -0300

    temp: add project_1

--- project_1/index.js
+++ project_1/index.js
@@ -1,29 +1,29 @@
 const path = require("path");
 
-const { readFilePromised, readDirPromised, srtFormat } = require("./utils");
+const {
+  readDirPromised,
+  getStrFiles,
+  readAllStrFiles,
+  toStringArr,
+  getWordsOnly,
+  toLowerCaseArr,
+  mapWordInObj,
+  groupByWord,
+  toCapitalizeWordArr,
+} = require("./utils");
 
 const pathSubtitles = path.join(__dirname, "../data/legendas");
 
-const getStrFiles = (filesNameArr = []) =>
-  filesNameArr.filter((fileName) => !!fileName.match(srtFormat));
-
-const toStringArr = (arr = []) => arr.map((el) => el.toString());
-
-const readAllStrFiles = (dirPath, fileNames = []) => {
-  const readFilesPromises = fileNames.map((filename) => {
-    const filePath = path.join(dirPath, filename);
-
-    return new Promise((resolve, reject) => {
-      readFilePromised(filePath).then(resolve).catch(reject);
-    });
-  });
-
-  return readFilesPromises;
-};
-
 readDirPromised(pathSubtitles)
-  .then((files) => getStrFiles(files))
-  .then((fileNames) => readAllStrFiles(pathSubtitles, fileNames))
+  .then(getStrFiles)
+  .then(strFiles => readAllStrFiles(pathSubtitles, strFiles))
   .then((readFilesPromises) => Promise.all(readFilesPromises))
-  .then((subtitlesArr) => toStringArr(subtitlesArr))
+  .then(toStringArr)
+  .then(getWordsOnly)
+  .then((wordsOnly) => wordsOnly.flat())
+  .then(toLowerCaseArr)
+  .then(mapWordInObj)
+  .then(groupByWord)
+  .then(toCapitalizeWordArr)
+  .then(console.log)
   .catch(console.log);
--- project_1/utils.js
+++ project_1/utils.js
@@ -1,7 +1,10 @@
 const fs = require("fs");
+const path = require("path");
+const ld = require("lodash");
 
 // regex
-const srtFormat = /^.*\.(srt)$/gi;
+const STR_FORMAT = /^.*\.(srt)$/gi;
+const WORD_ONLY = /(?<!(<\/))((?<!(\s<))(\b([a-zA-Z'])+\b))/gi;
 
 // common
 const readFilePromised = (path, options = {}) => {
@@ -20,8 +23,49 @@ const readDirPromised = (path, options = {}) => {
   });
 };
 
+const readAllStrFiles = (pathSubtitles, fileNames = []) => {
+  const readFilesPromises = fileNames.map((filename) => {
+    const filePath = path.join(pathSubtitles, filename);
+
+    return new Promise((resolve, reject) => {
+      readFilePromised(filePath).then(resolve).catch(reject);
+    });
+  });
+
+  return readFilesPromises;
+};
+
+// get
+
+const getStrFiles = (filesNameArr = []) =>
+  filesNameArr.filter((fileName) => !!fileName.match(STR_FORMAT));
+
+const getWordsOnly = (stringArr = []) =>
+  stringArr.map((text) => text.match(WORD_ONLY));
+
+// map
+const mapWordInObj = (wordsOnlyArr = []) =>
+  wordsOnlyArr.map((word) => ({ word }));
+
+const groupByWord = (arrWordInObj = []) =>
+  ld
+    .values(ld.groupBy(arrWordInObj, "word"))
+    .map((group) => ({ ...group[0], qty: group.length }));
+
+// to
+const toStringArr = (arr = []) => arr.map((text) => text.toString());
+const toLowerCaseArr = (arr = []) => arr.map((text = "") => text.toLowerCase());
+const toCapitalizeWordArr = (arr = []) =>
+  arr.map((el) => ({ ...el, word: ld.capitalize(el.word) }));
+
 module.exports = {
-  readFilePromised,
   readDirPromised,
-  srtFormat,
+  getStrFiles,
+  readAllStrFiles,
+  toStringArr,
+  getWordsOnly,
+  toLowerCaseArr,
+  mapWordInObj,
+  groupByWord,
+  toCapitalizeWordArr,
 };
